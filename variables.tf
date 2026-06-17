0bab8ed4a6bffa6e84d69093f3ca741f575373d3
raj-830803 <rajarshimu@google.com>
2026-06-17 11:38:50 +0000

    First

---
+++ variables.tf
@@ -0,0 +1,61 @@
+variable "gcp_project_id" {
+  description = "The GCP project ID."
+  type        = string
+
+}
+
+variable "helm_release_name" {
+  description = "helm release name."
+  type        = string
+  
+}
+
+variable "artifact_registry_repo_name" {
+  description = "OCI Image Name."
+  type        = string
+  default     = "oci-images"
+
+}
+
+variable "repository_link" {
+  type        = string
+  description = "HTTP repository link. Leaves empty to default to Google Artifact Registry."
+  default     = null #"https://helm.releases.hashicorp.com"
+}
+
+
+variable "gcp_region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "gcp_location" {
+  description = "GKE Cluster lcoation."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_id" {
+  description = "ID of the cluster"
+  type        = string
+}
+
+variable "helm_namespace" {
+  description = "GKE name space"
+  type        = string
+  default     = "vault1"
+}
+
+variable "chart" {
+  description = "Location of helm chart"
+  type        = string
+  default = "vault"
+}
+
+variable "set_inputs" {
+  description = "List of set inputs (e.g. name1:val1,name2:value2)"
+  type        = string
+  default     = "replicaCount:1"
+}
+
